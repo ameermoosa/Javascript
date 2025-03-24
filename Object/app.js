22b3b09a07d81bde735bef0198a83164e2b13453
Ameer Moosa <musameer488@gmail.com>
2025-03-24 14:50:19 +0500

    Add files via upload

---
+++ Object/app.js
@@ -0,0 +1,24 @@
+// Object///
+
+const studentData = {
+    zeroOne: "Ahmad",
+    zeroTwo: "Anus",
+    zeroThree: "Moosa",
+    zeroFour: "Farhan",
+    zeroFive: "AbdulBasit",
+    zeroSix: "Ahad",
+    zeroSeven: "Shoaib",
+    zeroEight: "Hassan",
+    zeroNine: "Hamza",
+    zeroTen: "Junaid",
+}
+console.log(studentData.zeroFive);
+
+
+
+const data ={
+    name : "AmeerMoosa",
+    phoneNumber : 923460882734,
+    remarks : "78%"
+}
+console.log(data)
